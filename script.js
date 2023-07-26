0094be95a80723d7136d06a9295c9d3b3fcf0efe
Nicat477 <nicatrzazad30@gmail.com>
2023-07-26 19:04:54 +0300

    fifth

---
+++ script.js
@@ -0,0 +1,7 @@
+const arrows=document.querySelectorAll(".arrow");
+const movieList=document.querySelectorAll(".movie-list");
+arrows.forEach((arrow)=>{
+    arrow.addEventListener("click",function(){
+
+    })
+})
