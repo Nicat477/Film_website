3a1c4e193d72a1eebfa6798592fcd5a2c34d124e
Nicat477 <nicatrzazad30@gmail.com>
2023-07-26 19:46:38 +0300

    sixth

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const arrows=document.querySelectorAll(".arrow");
 const movieList=document.querySelectorAll(".movie-list");
-arrows.forEach((arrow)=>{
+arrows.forEach((arrow,i)=>{
     arrow.addEventListener("click",function(){
-
+        movieList[i].computedStyleMap.transform=`translateX(${movieList[i].computedStyleMap().get("transform")[0].x.value}px)`
     })
 })
